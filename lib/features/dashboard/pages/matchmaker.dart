c1cdfe4bee33c6444e34e491dd1e0be505d3608b
MonaTheDon <e21cseu0656@bennett.edu.in>
2023-11-18 06:54:59 +0530

    matchmaker

--- lib/features/dashboard/pages/matchmaker.dart
+++ lib/features/dashboard/pages/matchmaker.dart
@@ -42,87 +42,89 @@ class _MatchMakerState extends State<MatchMaker> {
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Ingredient Match Maker",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 36.sp,
-                fontWeight: FontWeight.w500,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Ingredient Match Maker",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 36.sp,
+                  fontWeight: FontWeight.w500,
+                  color: white,
+                ),
+              ),
+              v(height: 10.h),
+              const Divider(
                 color: white,
+                thickness: 2,
               ),
-            ),
-            v(height: 10.h),
-            const Divider(
-              color: white,
-              thickness: 2,
-            ),
-            v(height: 12.h),
-            ingredientController.text.isEmpty
-                ? Text(
-                    "Discover tasty pairings for your ingredient! Search and savor the flavor combo here.",
-                    style: TextStyle(
-                      fontSize: 15.sp,
-                      fontWeight: FontWeight.w500,
-                      color: white,
-                    ),
-                  )
-                : const SizedBox(),
-            v(height: 20.h),
-            SizedBox(
-              height: 40.h,
-              child: TextField(
-                controller: ingredientController,
-                textAlignVertical: TextAlignVertical.center,
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: white,
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(8.r)),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(8.r)),
-                    hintText: "Type Ingredient Name",
-                    alignLabelWithHint: true,
-                    hintStyle: TextStyle(
-                      fontSize: 16.sp,
-                      color: hintGrey,
-                    )),
-                onChanged: (value) {
-                  setState(() {});
-                  debugPrint(value);
-                },
-                onSubmitted: (value) {
-                  getEntitiesForText(value);
-                },
-              ),
-            ),
-            v(height: 30.sp),
-            _isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(
-                      color: white,
-                    ),
-                  )
-                : entities.isNotEmpty
-                    ? ListView.separated(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (ctx, i) {
-                          return IngredientCard(entity: entities[i]);
-                        },
-                        separatorBuilder: (ctx, i) {
-                          return v(height: 5.h);
-                        },
-                        itemCount: entities.length,
-                      )
-                    : const Center(
-                        child: Text("No Matched Ingredients found"),
+              v(height: 12.h),
+              ingredientController.text.isEmpty
+                  ? Text(
+                      "Discover tasty pairings for your ingredient! Search and savor the flavor combo here.",
+                      style: TextStyle(
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w500,
+                        color: white,
                       ),
-          ],
+                    )
+                  : const SizedBox(),
+              v(height: 20.h),
+              SizedBox(
+                height: 40.h,
+                child: TextField(
+                  controller: ingredientController,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: white,
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(8.r)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(8.r)),
+                      hintText: "Type Ingredient Name",
+                      alignLabelWithHint: true,
+                      hintStyle: TextStyle(
+                        fontSize: 16.sp,
+                        color: hintGrey,
+                      )),
+                  onChanged: (value) {
+                    setState(() {});
+                    debugPrint(value);
+                  },
+                  onSubmitted: (value) {
+                    getEntitiesForText(value);
+                  },
+                ),
+              ),
+              v(height: 30.sp),
+              _isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                        color: white,
+                      ),
+                    )
+                  : entities.isNotEmpty
+                      ? ListView.separated(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (ctx, i) {
+                            return IngredientCard(entity: entities[i]);
+                          },
+                          separatorBuilder: (ctx, i) {
+                            return v(height: 5.h);
+                          },
+                          itemCount: entities.length,
+                        )
+                      : const Center(
+                          child: Text("No Matched Ingredients found"),
+                        ),
+            ],
+          ),
         ),
       )),
     );
