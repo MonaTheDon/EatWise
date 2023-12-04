5b67e9d054aa6b73335e8e90021ebb3fd4ff5a96
MonaTheDon <e21cseu0656@bennett.edu.in>
2023-12-04 21:06:43 +0530

    profile

--- lib/features/dashboard/pages/profile.dart
+++ lib/features/dashboard/pages/profile.dart
@@ -11,45 +11,79 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            
-            Text(
-              "My Profile",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 36.sp,
-                fontWeight: FontWeight.w500,
-                color: white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              v(height: 15.h),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.w),
+                child: Text(
+                  "My Profile",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 36.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xffe417E45),
+                  ),
+                ),
               ),
-            ),
-            v(height: 10.h),
-            const Divider(
-              color: white,
-              thickness: 2,
-            ),
-            Icon(MdiIcons.account,
-            size: 70.sp,
-            color: grey,),
-            // v(height: 1.sp),
-            Text("Altamash",
-            textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 36.sp,
-                fontWeight: FontWeight.w500,
-                color: white,
+              v(height: 8.h),
+               Divider(
+                color: Color(0xffeD9D9D9),
+                thickness: 2,
+                indent: 12.w,
+                endIndent: 12.w,
               ),
-            ),
-            v(height: 2.sp),
-            const Divider(
-              color: white,
-              thickness: 2,
-            ),
-          ]
-        )
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Icon(MdiIcons.account,
+                size: 100.sp,
+                color: grey,),
+              ),
+              // v(height: 1.sp),
+          
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                
+                child: Column(
+                  children: [
+                    Text("@mona123",
+                    style: TextStyle(
+                        fontSize: 15.sp,
+                        
+                        color: Color(0xffe417E45),
+                      ),
+                    ),
+                    v(height: 5.h),
+          
+                    Text("Tanisha Singh",
+                    textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 28.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xffe417E45),
+                      ),
+                    ),
+                    Text("India",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 15.sp,
+                        
+                        color: Color(0xffe417E45),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              v(height: 2.sp),
+              Expanded(
+                child: Container(
+                  color: Color(0xffeD9D9D9),
+                ),
+              )
+            ]
           )
       ),
     );
