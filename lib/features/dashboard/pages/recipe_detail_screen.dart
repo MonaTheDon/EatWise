a00a6ff535eeaf128060cec18bc2345295935577
altmxx <altamashalam19@gmail.com>
2023-12-04 19:26:08 +0530

    Refactor recipe card widget

--- lib/features/dashboard/pages/recipe_detail_screen.dart
+++ lib/features/dashboard/pages/recipe_detail_screen.dart
@@ -59,145 +59,283 @@ class _RecipeInfoScreenState extends State<RecipeInfoScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    // return Scaffold(
+    //   body: SafeArea(
+    //     child: SingleChildScrollView(
+    //       child: Column(
+    //         children: [
+    //           Text(
+    //             widget.recipe.recipeName!,
+    //             textAlign: TextAlign.center,
+    //             maxLines: 2,
+    //             overflow: TextOverflow.ellipsis,
+    //             style: TextStyle(
+    //               fontSize: 28.sp,
+    //               fontWeight: FontWeight.w500,
+    //               color: black,
+    //             ),
+    //           ),
+    //           v(height: 8.h),
+    //           const Divider(
+    //             color: white,
+    //             thickness: 1,
+    //           ),
+    //           v(height: 8.h),
+    //           Container(
+    //             width: 360.w,
+    //             height: 270.h,
+    //             decoration: BoxDecoration(
+    //               image: DecorationImage(
+    //                   image: NetworkImage(widget.recipe.imageUrl!),
+    //                   fit: BoxFit.cover),
+    //             ),
+    //           ),
+    //           v(height: 12.h),
+    //           Row(
+    //             mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //             children: [
+    //               GestureDetector(
+    //                 onTap: () {
+    //                   setState(() {
+    //                     selectedOption = "ingredients";
+    //                   });
+    //                 },
+    //                 child: Container(
+    //                   padding:
+    //                       EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
+    //                   decoration: BoxDecoration(
+    //                     color: darkGreen,
+    //                     borderRadius: BorderRadius.circular(5.r),
+    //                     border: Border.all(
+    //                       color: selectedOption == "ingredients"
+    //                           ? white
+    //                           : darkGreen,
+    //                       width: 2,
+    //                     ),
+    //                   ),
+    //                   child: Text(
+    //                     "Ingredients",
+    //                     style: TextStyle(
+    //                       fontSize: 16.sp,
+    //                       fontWeight: FontWeight.w500,
+    //                       color: white,
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ),
+    //               h(width: 8.w),
+    //               GestureDetector(
+    //                 onTap: () {
+    //                   setState(() {
+    //                     selectedOption = "reviews";
+    //                   });
+    //                 },
+    //                 child: Container(
+    //                   padding:
+    //                       EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
+    //                   decoration: BoxDecoration(
+    //                     color: darkGreen,
+    //                     borderRadius: BorderRadius.circular(5.r),
+    //                     border: Border.all(
+    //                         color:
+    //                             selectedOption == "reviews" ? white : darkGreen,
+    //                         width: 2),
+    //                   ),
+    //                   child: Text(
+    //                     "Reviews",
+    //                     style: TextStyle(
+    //                       fontSize: 16.sp,
+    //                       fontWeight: FontWeight.w500,
+    //                       color: white,
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ),
+    //               h(width: 8.w),
+    //               GestureDetector(
+    //                 onTap: () {
+    //                   setState(() {
+    //                     selectedOption = "procedure";
+    //                   });
+    //                 },
+    //                 child: Container(
+    //                   padding:
+    //                       EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
+    //                   decoration: BoxDecoration(
+    //                     color: darkGreen,
+    //                     borderRadius: BorderRadius.circular(5.r),
+    //                     border: Border.all(
+    //                         color: selectedOption == "procedure"
+    //                             ? white
+    //                             : darkGreen,
+    //                         width: 2),
+    //                   ),
+    //                   child: Text(
+    //                     "Procedure",
+    //                     style: TextStyle(
+    //                       fontSize: 16.sp,
+    //                       fontWeight: FontWeight.w500,
+    //                       color: white,
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ),
+    //             ],
+    //           ),
+    //           v(height: 12.h),
+    //           selectedOption == "ingredients"
+    //               ? _isIngredientPhraseLoading
+    //                   ? const CircularProgressIndicator(color: white)
+    //                   : IngredientsWidget(ingredientPhrases: ingredientPhrases)
+    //               : selectedOption == "procedure"
+    //                   ? _isProcedureLoading
+    //                       ? const CircularProgressIndicator(color: white)
+    //                       : ProcedureWidget(procedure: procedure)
+    //                   : selectedOption == "reviews"
+    //                       ? ReviewWidget()
+    //                       : Container(),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
           child: Column(
-            children: [
-              Text(
-                widget.recipe.recipeName!,
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 28.sp,
-                  fontWeight: FontWeight.w500,
-                  color: white,
-                ),
-              ),
-              v(height: 8.h),
-              const Divider(
-                color: white,
-                thickness: 1,
-              ),
-              v(height: 8.h),
-              Container(
-                width: 360.w,
-                height: 270.h,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(widget.recipe.imageUrl!),
-                      fit: BoxFit.cover),
-                ),
-              ),
-              v(height: 12.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedOption = "ingredients";
-                      });
-                    },
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
-                      decoration: BoxDecoration(
-                        color: darkGreen,
-                        borderRadius: BorderRadius.circular(5.r),
-                        border: Border.all(
-                          color: selectedOption == "ingredients"
-                              ? white
-                              : darkGreen,
-                          width: 2,
-                        ),
-                      ),
-                      child: Text(
-                        "Ingredients",
-                        style: TextStyle(
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w500,
-                          color: white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  h(width: 8.w),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedOption = "reviews";
-                      });
-                    },
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
-                      decoration: BoxDecoration(
-                        color: darkGreen,
-                        borderRadius: BorderRadius.circular(5.r),
-                        border: Border.all(
-                            color:
-                                selectedOption == "reviews" ? white : darkGreen,
-                            width: 2),
-                      ),
-                      child: Text(
-                        "Reviews",
-                        style: TextStyle(
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w500,
-                          color: white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  h(width: 8.w),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedOption = "procedure";
-                      });
-                    },
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
-                      decoration: BoxDecoration(
-                        color: darkGreen,
-                        borderRadius: BorderRadius.circular(5.r),
-                        border: Border.all(
-                            color: selectedOption == "procedure"
-                                ? white
-                                : darkGreen,
-                            width: 2),
-                      ),
-                      child: Text(
-                        "Procedure",
-                        style: TextStyle(
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w500,
-                          color: white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              v(height: 12.h),
-              selectedOption == "ingredients"
-                  ? _isIngredientPhraseLoading
-                      ? const CircularProgressIndicator(color: white)
-                      : IngredientsWidget(ingredientPhrases: ingredientPhrases)
-                  : selectedOption == "procedure"
-                      ? _isProcedureLoading
-                          ? const CircularProgressIndicator(color: white)
-                          : ProcedureWidget(procedure: procedure)
-                      : selectedOption == "reviews"
-                          ? ReviewWidget()
-                          : Container(),
-            ],
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300.h,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(widget.recipe.imageUrl!),
+                  fit: BoxFit.cover),
+            ),
           ),
-        ),
-      ),
+          v(height: 12.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 216.w,
+                  child: Text(
+                    widget.recipe.recipeName!,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 28.sp,
+                      fontWeight: FontWeight.w500,
+                      color: black,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
+                    size: 32,
+                  ),
+                )
+              ],
+            ),
+          ),
+          v(height: 12.h),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Text(
+                "${widget.recipe.cookTime} minutes • Serves ${widget.recipe.servings} • 12 Reviews",
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  color: grey,
+                ),
+              ),
+            ),
+          ),
+          v(height: 15.h),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: size.width / 3,
+                decoration: BoxDecoration(
+                  color: white,
+                  border: const Border(
+                    bottom: BorderSide(
+                      color: backgroundGreen,
+                      width: 2,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(5.r),
+                    topRight: Radius.circular(5.r),
+                  ),
+                ),
+                child: Text(
+                  "Cookware",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: backgroundGreen,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: size.width / 3,
+                decoration: BoxDecoration(
+                  color: backgroundGreen,
+                  border: const Border(
+                    bottom: BorderSide(
+                      color: backgroundGreen,
+                      width: 2,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(5.r),
+                    topRight: Radius.circular(5.r),
+                  ),
+                ),
+                child: Text(
+                  "Ingredients",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: white,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: size.width / 3,
+                decoration: BoxDecoration(
+                  color: white,
+                  border: const Border(
+                    bottom: BorderSide(
+                      color: backgroundGreen,
+                      width: 2,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(5.r),
+                    topRight: Radius.circular(5.r),
+                  ),
+                ),
+                child: Text(
+                  "Instructions",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: backgroundGreen,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      )),
     );
   }
 }
